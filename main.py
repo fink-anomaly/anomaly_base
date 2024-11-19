f82bb37078e8f648b51b1628c0122482a02cc3c2
Oxpa <iippolitov@gmail.com>
2024-11-19 18:03:27 +0000

    Don't use low ports

--- main.py
+++ main.py
@@ -320,4 +320,4 @@ if __name__ == '__main__':
             ssl_keyfile=keypath,
             ssl_certfile=certpath)
     else:
-        uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
+        uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=False)
