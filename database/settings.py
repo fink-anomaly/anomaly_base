591e13b360535e717480a9cf409a135308aaf326
oxpa <iippolitov@gmail.com>
2025-01-06 11:37:41 +0000

    Initial sql backend implementation.

--- database/settings.py
+++ database/settings.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from typing import Optional
 from database.mongo import initialize_database as mongo_init
 
@@ -6,6 +6,7 @@ class Settings(BaseSettings):
     DATABASE_URL: Optional[str] = None
     SECRET_KEY: Optional[str] = None
     TG_TOKEN: Optional[str] = None
+    DEBUG: Optional[str] = None
 
     def is_mongo(self):
         return self.DATABASE_URL.startswith("mongodb://")
@@ -13,6 +14,13 @@ class Settings(BaseSettings):
     async def initialize_database(self):
         if self.DATABASE_URL.startswith("mongodb://"):
             await mongo_init(self)
+        else:
+            from sqlmodel import create_engine
+            from database.sqlalchemy import Base
+            engine = create_engine(self.DATABASE_URL, echo=not not self.DEBUG )
+            Base.engine = engine
+            Base.metadata.create_all(engine)
 
     class Config:
         env_file = ".env"
+        extra = 'allow'
--- models/base_types.py
+++ models/base_types.py
@@ -1,4 +1,6 @@
 from database.settings import Settings
 
 if Settings().is_mongo():
-    from  models.mongo import ImageDocument, reaction, update_reaction, User, TokenResponse, ObjectId
+    from models.mongo import ImageDocument, reaction, update_reaction, User, TokenResponse, ObjectId
+else:
+    from models.sqlalchemy import ImageDocument, reaction, update_reaction, User, TokenResponse, ObjectId
